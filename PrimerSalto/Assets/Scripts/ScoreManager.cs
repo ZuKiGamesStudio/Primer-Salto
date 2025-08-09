15ef04017badcbd2f32355870e36be048c8048f2
Zuki <zuki.games.studio@gmail.com>
2025-08-09 14:08:18 -0300

    highscore

--- PrimerSalto/Assets/Scripts/ScoreManager.cs
+++ PrimerSalto/Assets/Scripts/ScoreManager.cs
@@ -1,18 +1,37 @@
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
     public Transform player;
     public TMP_Text scoreText;
+    public TMP_Text highScoreText;
 
     private float score = 0f;
+    private int highScore = 0;
+
+    void Start()
+    {
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+        UpdateHighScoreText();
+    }
 
     void Update()
     {
-        // La puntuación será la posición X del jugador (distancia recorrida)
         score = player.position.x;
         scoreText.text = "Score: " + Mathf.FloorToInt(score).ToString();
+
+        if (score > highScore)
+        {
+            highScore = Mathf.FloorToInt(score);
+            PlayerPrefs.SetInt("HighScore", highScore);
+            UpdateHighScoreText();
+        }
+    }
+
+    void UpdateHighScoreText()
+    {
+        if (highScoreText != null)
+            highScoreText.text = "High Score: " + highScore.ToString();
     }
 }
